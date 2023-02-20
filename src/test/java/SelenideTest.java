39d12f6e80640cc670e7a1b795bcc849478c66a9
Maria Ustyuzhanina <elkaran@gmail.com>
2023-02-20 19:23:58 +0300

    Trying to catch an error

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,6 +1,8 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest {
@@ -9,11 +11,12 @@ public class SelenideTest {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("[data-test-id='city']").setValue("Омск");
-        $x("//input[@placeholder="Дата встречи"]").setValue("15.02.2023");
+        $x("//input[@placeholder='Дата встречи']").setValue("25.02.2023");
         $("[data-test-id='name']").setValue("Маруся Кошкина");
         $("[data-test-id='phone']").setValue("+79991112233");
         $x("//label").click();
         $x("//span[text()='Забронировать']").click();
+        $x("//*[@class='notification__title']").shouldHave(Condition.text("Успешно!"));
 
 
     }
